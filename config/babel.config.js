ea0b04cbc4bd5a391cb879dca2115d5b8a81a280
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-27 20:44:39 -0500

    update configuration for better shimming
    created shared babel config file to avoid confusion between configuring babel for rollup and configuring babel for babel.

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,15 +1,10 @@
+const { babelPresets, babelPlugins } = require('./babel-shared.config.js');
+
 module.exports = function (api) {
   api.cache.invalidate(() => process.env.NODE_ENV === "production");
 
-  const presets = [
-    ['@babel/preset-env', {
-      useBuiltIns: 'usage'
-    }],
-    '@babel/preset-react' ];
-  const plugins = [ "@babel/plugin-proposal-class-properties" ];
-
   return {
-    presets,
-    plugins
+    presets: babelPresets,
+    plugins: babelPlugins
   };
 }
