55b2d0ce3d8b713b5bfd2bbb71c46e3ec3f3911c
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-08 17:02:22 -0500

    moved 'pretest' logic to it's own lib; added services check prior to integrations test

--- src/catalyst-scripts.sh
+++ src/catalyst-scripts.sh
@@ -10,6 +10,7 @@ ACTION="$1"; shift
 import echoerr
 import find_exec
 import lists
+source ./lib/pretest.sh
 source ./lib/test.sh
 
 CONFIG_PATH="$(npm explore @liquid-labs/catalyst-scripts -- pwd)/config"
@@ -59,29 +60,7 @@ case "$ACTION" in
     fi
   ;;
   pretest)
-    if [[ -d 'go' ]]; then
-      if ! data_reset; then
-        COMMAND="${COMMAND}echo 'Skippeng DB reset.';"
-      elif test_integration && [[ -n "$(find go -name "sql.go" -print -quit)" ]]; then
-        # Because go generally builds faster than DBs reset, we check the build
-        # first to avoid possible costly and pointless DB reset.
-        COMMAND="${COMMAND}echo 'Complie check...'; cd go && go build ./...; cd ..;"
-        # Rebuild the schema
-        COMMAND="${COMMAND}"'echo "Ressetting database..."; catalyst data rebuild sql || ( EXIT=$?; echo -e "If you want to run only unit tests, you can invoke the NPM command like\nTEST_TYPES=unit npm run test"; exit $EXIT );'
-        # Load test data (if any)
-        if [[ -d "./data/sql/test" ]]; then
-          COMMAND="${COMMAND}catalyst data load test;"
-        else
-          echo "No test data files found."
-        fi
-      fi
-    fi
-    if [[ -d 'js' ]]; then
-      BABEL=`require-exec babel`
-      BABEL_CONFIG="${CONFIG_PATH}/babel.config.js"
-      # Jest is not picking up the external maps, so we inline them for the test.
-      COMMAND="${COMMAND}rm -rf test-staging; ${BABEL} --config-file ${BABEL_CONFIG} ./js --out-dir test-staging --source-maps=inline"
-    fi
+    catalyst_pretest
   ;;
   test)
     catalyst_test
