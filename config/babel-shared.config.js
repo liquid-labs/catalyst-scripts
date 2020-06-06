ecca1bcd7b284ddf251460fc500efac27ee61b62
Zane Rockenbaugh <zane@liquid-labs.com>
2020-06-06 04:00:01 -0500

    hotfix: update packages
    I let myself get pulled into a long detour when I tried to get too fancy and had private methods; it was good to see babel update was pretty easy, but there were gotchas and I kept chasing them down, only to give it up in the end.

--- config/babel-shared.config.js
+++ config/babel-shared.config.js
@@ -8,6 +8,12 @@ exports.rollupBabelPresets = [
   '@babel/preset-react'
 ];
 
+// NOTE: We've tried a couple times to add 'private methods'. The problem comes in that classic conventions like
+// 'this[fieldName]' fail (or at least might, never fully debugged). Dynamic field access and just general complication
+// mean we should avoid. Besides, with the use of modules, there's an easy solution: create a function that's not
+// exported outside the class.
+// NOTE: We tried 'throw expressions', but the resulting code did not seem logically consistent with the source code.
+// This certainly could have been user error, but simpler to just avoid it for now.
 exports.babelPlugins = [
   '@babel/plugin-proposal-class-properties',
   '@babel/plugin-proposal-optional-chaining',
